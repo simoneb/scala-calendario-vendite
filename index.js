5fbeb0bca79449a305d9e8796d59892feae195b7
Simone Busoli <simone.busoli@gmail.com>
2018-03-08 12:39:29 +0100

    scala calendario vendite

--- index.js
+++ index.js
@@ -2,34 +2,45 @@ const request = require('superagent')
 const cheerio = require('cheerio')
 const ical = require('ical-generator')
 const http = require('http')
+const moment = require('moment')
 
-cal = ical({domain: 'iteatri.re.it', name: 'Teatri Reggio', ttl: 60 * 60 /* 1 hour*/})
+moment.locale('IT-it')
+
+cal = ical({
+  domain: 'www.teatroallascala.org',
+  name: 'Scala Calendario Vendite',
+  ttl: 60 * 60 /* 1 hour*/
+})
 
 http.createServer((req, res) => serveCalendar(res)).listen(process.env.PORT || 3000)
 
 function serveCalendar (httpRes) {
-  request.get('http://www.iteatri.re.it/Calendario.jsp')
+  request.get('http://www.teatroallascala.org/it/biglietteria/biglietti-tariffe/calendario-vendite.html')
     .type('text/html')
     .end((err, res) => {
       if (err) throw err
       const $ = cheerio.load(res.text)
-      const script = $('#calendarioEventi').find('script').text()
-      const [, eventsJsString] = /events: (\[[\s\S.]+}\,\s+\])/.exec(script)
+
+      const events = $('table')
+        .find('tr')
+        .filter((_, e) => /\d{4}/.test($(e).find('td').eq(1).find('p').text()))
+        .map((_, e) => ({
+          name: $(e).find('td').eq(0).find('p').text(),
+          date: moment($(e).find('td').eq(1).find('p').text(), 'dddd DD MMMM YYYY')
+        })).toArray()
 
       cal.events().length = 0
 
       cal.events(
-        eval(eventsJsString)
+        events
           .map(event => ({
-            start: new Date(event.start),
-            end: new Date(event.end),
-            summary: event.title,
-            description: event.title,
-            location: event.luogo,
-            url: event.url,
+            start: event.date.toDate(),
+            // end: new Date(event.end),
+            summary: event.name,
+            description: event.name,
             allDay: true
           }))
-          .filter(event => (Math.abs(event.end - event.start) / 1000 / 60 / 60 / 24) < 2)
+          // .filter(event => (Math.abs(event.end - event.start) / 1000 / 60 / 60 / 24) < 2)
       )
 
       cal.serve(httpRes)
